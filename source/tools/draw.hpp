ca9b08a364753bb6e0d2cb654974e9b93a3b2aea
Cole Scheller <schelcc@pobox.com>
2026-06-15 10:28:17 -0400

    Add text helper, coloring helper

--- source/tools/draw.hpp
+++ source/tools/draw.hpp
@@ -1,8 +1,10 @@
 #pragma once
+#include "core.hpp"
 #include "tools/numeric.hpp"
 #include <cassert>
 #include <cmath>
 #include <memory>
+#include <ncpp/Palette.hh>
 #include <ncpp/Plane.hh>
 #include <notcurses/notcurses.h>
 #include <string_view>
@@ -42,31 +44,55 @@ static inline std::wstring prog_bar(double pct, size_t chars) {
   return bar;
 }
 
-static inline void set_row_bg_rgba(std::shared_ptr<ncpp::Plane> p,
-                                   size_t const row, unsigned const r,
-                                   unsigned const g, unsigned const b,
-                                   unsigned const a = 2) {
-  // Alpha's channel is 2 bit, but bg forbids alpha = 3
-  assert(a < 3);
+enum class PaletteColors {
+  RED_HARD = 0,
+  GREEN_HARD = 1,
+  YELLOW_HARD = 2,
+  BLUE_HARD = 3,
+  PURPLE_HARD = 4,
+  CYAN_HARD = 5,
+  LIGHT_GRAY_HARD = 6,
+  DARK_GRAY_HARD = 7,
+  GREEN_SOFT = 8,
+  YELLOW_SOFT = 9,
+  BLUE_SOFT = 10,
+  PURPLE_SOFT = 11,
+  CYAN_SOFT = 12,
+  LIGHT_GRAY_SOFT = 13,
+  DARK_GRAY_SOFT = 14,
+};
 
-  // For some godforsaken reason ncchannel_set_alpha expects the 2 alpha bits to
-  // be the two most significant bits, so we have to shift the alpha value up to
-  // match
-  unsigned alpha = a << 28;
+enum class Target {
+  FG,
+  BG,
+};
 
+static inline void set_row_color(std::shared_ptr<ncpp::Plane> p,
+                                 size_t const row, Target const target,
+                                 PaletteColors const color) {
   uint64_t chan = p->get_channels();
-  assert(ncchannels_set_bg_rgb8(&chan, r, g, b) == 0);
-  assert(ncchannels_set_bg_alpha(&chan, alpha) == 0);
+
+  ncpp::Palette pal{};
+
+  unsigned int r;
+  unsigned int g;
+  unsigned int b;
+
+  pal.get(static_cast<int>(color), r, g, b);
+
+  if (target == Target::FG)
+    ncchannels_set_fg_rgb8(&chan, r, g, b);
+  else
+    ncchannels_set_bg_rgb8(&chan, r, g, b);
 
   p->stain(row, 1, 1, p->get_dim_x() - 2, chan, chan, chan, chan);
+}
 
-  // Start at x=1 and stop before dim_x - 1 to avoid borders
-  // for (unsigned int x = 1; x < p->get_dim_x() - 1; x++) {
-  //   ncpp::Cell cell{};
-  //   p->get_at(row, x, &cell);
-  //   cell.set_bg_alpha(a);
-  //   cell.set_bg_rgb8(r, g, b);
-  // }
+static inline std::string trunc_str(std::string_view const s,
+                                    size_t const max_len) {
+  return s.length() <= (max_len - 3)
+             ? std::string{s}
+             : std::string{s.substr(0, max_len)} + "...";
 }
 
 }; // namespace Tools::Draw
