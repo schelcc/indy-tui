8ee802f36e79055b6e2ff15392bc370fed8fced9
Cole Scheller <schelcc@pobox.com>
2026-08-07 10:40:09 -0400

    Add served replay CLI options and operation mode

--- source/client_main.cpp
+++ source/client_main.cpp
@@ -2,9 +2,11 @@
 #include <cstdlib>
 #include <cstring>
 #include <ctime>
+#include <filesystem>
 #include <optional>
 #include <print>
 #include <pthread.h>
+#include <string>
 #include <thread>
 #include <vector>
 
@@ -43,7 +45,7 @@ int main([[maybe_unused]] const int argc, [[maybe_unused]] const char *argv[]) {
   parser.add_element<Arg>("mode")
       .set_help_msg("What mode of operation to use. Options are 'live', "
                     "'live-debug', and "
-                    "'replay'.")
+                    "'local-replay'.")
       .set_handler([](std::string_view const s) -> Core::SessionSource {
         if (s == "live")
           return Core::SessionSource::SERVED_REMOTE;
@@ -55,6 +57,73 @@ int main([[maybe_unused]] const int argc, [[maybe_unused]] const char *argv[]) {
           throw CLI::InputParsingErr(
               std::format("Invalid input for mode: '{}'", s));
       });
+
+  parser.add_element<Option>("source")
+      .set_help_msg("Path to the telemetry session to replay")
+      .set_long_opt("source")
+      .set_short_opt("s")
+      .set_handler([](std::string_view const s) -> std::string {
+        std::filesystem::path path(s);
+
+        assert(s.length() > 0);
+
+        if (path.is_relative()) {
+          path = std::filesystem::current_path() / path;
+        }
+
+        if (!std::filesystem::exists(path))
+          throw CLI::InputParsingErr(
+              std::format("Session source '{}' does not exist", path.string()));
+
+        if (!std::filesystem::is_regular_file(path))
+          throw CLI::InputParsingErr(
+              std::format("Session source '{}' is not a file", path.string()));
+
+        if (!std::filesystem::is_regular_file(path))
+          throw CLI::InputParsingErr(
+              std::format("Session source '{}' is not a file", path.string()));
+
+        {
+          std::ifstream f(path.string(), std::ios::in);
+          if (!f.good())
+            throw CLI::InputParsingErr(std::format(
+                "Session source '{}' is not readable", path.string()));
+        }
+
+        return path.c_str();
+      });
+
+  parser.add_element<Flag>("serve replay")
+      .set_help_msg("Start a replay server. Blocks regular usage of the "
+                    "application. Requires -s/--source to be set.")
+      .set_long_opt("serve-replay")
+      .set_default_state(false);
+
+  parser.add_element<Option>("hostname")
+      .set_help_msg(
+          "Set the hostname to use for connecting to a remote served replay, "
+          "or for hosting a replay server. Defaults to '127.0.0.1'")
+      .set_long_opt("hostname")
+      .set_short_opt("H")
+      .set_handler(
+          [](std::string_view const s) -> std::string { return s.data(); })
+      .set_default_input("127.0.0.1");
+
+  parser.add_element<Option>("port")
+      .set_help_msg(
+          "Set the port to use for connecting to a remote served replay, for "
+          "for hosting a replay server. Defaults to '8080'")
+      .set_long_opt("port")
+      .set_short_opt("P")
+      .set_handler([](std::string_view const s) -> size_t {
+        try {
+          return std::stoul(s.data());
+        } catch (...) {
+          throw CLI::InputParsingErr(
+              std::format("Port '{}' is not a valid port", s));
+        }
+      })
+      .set_default_input("8080");
 
   parser.add_element<Option>("url")
       .set_help_msg("What websocket URL to reach for the 'live-debug' mode.")
@@ -110,6 +179,26 @@ int main([[maybe_unused]] const int argc, [[maybe_unused]] const char *argv[]) {
 
   Tools::Log::SetOut(parser.get<Option, std::string_view>("log output"));
   Tools::Log::SetLevel(parser.get<Option, Tools::Log::Level>("log level"));
+
+  if (parser.get<Flag, bool>("serve replay")) {
+    // Will not return to main from here
+    std::jthread server{[&parser](std::stop_token stop) {
+      Workers::ServedReplayWorker worker(
+          parser.get<Option, std::string>("source"),
+          parser.get<Option, std::string>("hostname"),
+          parser.get<Option, size_t>("port"));
+      worker(stop);
+    }};
+
+    App::AppContext::AwaitShutdown();
+
+    std::println("Shutdown requested for reason '{}'",
+                 App::AppContext::GetReason().value_or(
+                     "no reason provided -- ungraceful shutdown"));
+
+    server.request_stop();
+    return EXIT_SUCCESS;
+  }
 
   std::vector<ncpp::NCKey> key_queue{};
 
